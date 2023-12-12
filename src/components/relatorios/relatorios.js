5a26be1bdf95840f17ec410ee00c514508b4c21f
WesleyEstevam <wesleyestevam15@gmail.com>
2023-12-11 22:09:19 -0300

    mudança de rota

--- src/components/relatorios/relatorios.js
+++ src/components/relatorios/relatorios.js
@@ -1,149 +1,139 @@
-import { useState, useEffect } from 'react';
-import EditIcon from '@mui/icons-material/Edit';
-import InfoIcon from '@mui/icons-material/Info';
+import { useState, useEffect } from "react";
+import EditIcon from "@mui/icons-material/Edit";
+import InfoIcon from "@mui/icons-material/Info";
 import ImageList from "@mui/material/ImageList";
-import axios from 'axios'
+import axios from "axios";
 import {
-    Box,
-    Card,
-    Button,
-    Checkbox,
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TablePagination,
-    TableRow,
-    Typography
-} from '@mui/material';
-import { DeletarItem } from '../btn_acao/btn-delet';
-import { useRouter } from 'next/router'
-import { baseURL } from '../api/api';
+  Box,
+  Card,
+  Button,
+  Checkbox,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { DeletarItem } from "../btn_acao/btn-delet";
+import { useRouter } from "next/router";
+import { baseURL } from "../api/api";
 
 export const Relatorios = () => {
-    const [selectedCustomerIds, setSelectedCustomerIds] = useState([]);
-    const [limit, setLimit] = useState(10);
-    const [page, setPage] = useState(0);
-    const [morador, setMorador] = useState([])
-    const router = useRouter()
+  const [selectedCustomerIds, setSelectedCustomerIds] = useState([]);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
+  const [morador, setMorador] = useState([]);
+  const router = useRouter();
 
-    // LISTAGEM DE MORADORES
-    useEffect(() => {
-        axios.get(baseURL + "morador")
-            .then((response) => {
-                setMorador(response.data)
-            }).catch((error) => {
-                console.error(error)
-            })
-    }, [])
+  // LISTAGEM DE MORADORES
+  useEffect(() => {
+    axios
+      .get(baseURL + "visitas")
+      .then((response) => {
+        setMorador(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-    const handleSelectAll = (event) => {
-        let newSelectedCustomerIds;
+  const handleSelectAll = (event) => {
+    let newSelectedCustomerIds;
 
-        if (event.target.checked) {
-            newSelectedCustomerIds = morador.map((morador) => morador.id);
-        } else {
-            newSelectedCustomerIds = [];
-        }
+    if (event.target.checked) {
+      newSelectedCustomerIds = morador.map((morador) => morador.id);
+    } else {
+      newSelectedCustomerIds = [];
+    }
 
-        setSelectedCustomerIds(newSelectedCustomerIds);
-    };
+    setSelectedCustomerIds(newSelectedCustomerIds);
+  };
 
-    const handleSelectOne = (event, id) => {
-        const selectedIndex = selectedCustomerIds.indexOf(id);
-        let newSelectedCustomerIds = morador;
+  const handleSelectOne = (event, id) => {
+    const selectedIndex = selectedCustomerIds.indexOf(id);
+    let newSelectedCustomerIds = morador;
 
-        if (selectedIndex === -1) {
-            newSelectedCustomerIds = newSelectedCustomerIds.concat(selectedCustomerIds, id);
-        } else if (selectedIndex === 0) {
-            newSelectedCustomerIds = newSelectedCustomerIds.concat(selectedCustomerIds.slice(1));
-        } else if (selectedIndex === selectedCustomerIds.length - 1) {
-            newSelectedCustomerIds = newSelectedCustomerIds.concat(selectedCustomerIds.slice(0, -1));
-        } else if (selectedIndex > 0) {
-            newSelectedCustomerIds = newSelectedCustomerIds.concat(
-                selectedCustomerIds.slice(0, selectedIndex),
-                selectedCustomerIds.slice(selectedIndex + 1)
-            );
-        }
+    if (selectedIndex === -1) {
+      newSelectedCustomerIds = newSelectedCustomerIds.concat(
+        selectedCustomerIds,
+        id
+      );
+    } else if (selectedIndex === 0) {
+      newSelectedCustomerIds = newSelectedCustomerIds.concat(
+        selectedCustomerIds.slice(1)
+      );
+    } else if (selectedIndex === selectedCustomerIds.length - 1) {
+      newSelectedCustomerIds = newSelectedCustomerIds.concat(
+        selectedCustomerIds.slice(0, -1)
+      );
+    } else if (selectedIndex > 0) {
+      newSelectedCustomerIds = newSelectedCustomerIds.concat(
+        selectedCustomerIds.slice(0, selectedIndex),
+        selectedCustomerIds.slice(selectedIndex + 1)
+      );
+    }
 
-        setSelectedCustomerIds(newSelectedCustomerIds);
-    };
+    setSelectedCustomerIds(newSelectedCustomerIds);
+  };
 
-    const handleLimitChange = (event) => {
-        setLimit(event.target.value);
-    };
+  const handleLimitChange = (event) => {
+    setLimit(event.target.value);
+  };
 
-    const handlePageChange = (event, newPage) => {
-        setPage(newPage);
-    };
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);
+  };
 
-    return (
-        <Card>
-            <ImageList
-                sx={{
-                    gridAutoFlow: "column",
-                    gridTemplateColumns: "repeat(auto-fit, minmax(160px,1fr)) !important",
-                    gridAutoColumns: "minmax(160px, 1fr)"
-                }}
-            >
-                <Box >
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>
-                                    Nome
-                                </TableCell>
-                                <TableCell>
-                                    Documento
-                                </TableCell>
-                                <TableCell>
-                                    E-mail
-                                </TableCell>
-                                <TableCell>
-                                    Telefone
-                                </TableCell>
-                                <TableCell>
-                                    AP/Casa
-                                </TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {morador.slice(0, limit).map((morador) => (
-                                <TableRow
-                                    hover
-                                    key={morador.id}
-                                    selected={selectedCustomerIds.indexOf(morador.id) !== -1}
-                                >
-                                    <TableCell>
-                                        {morador.nomePessoa}
-                                    </TableCell>
-                                    <TableCell>
-                                        {morador.documento}
-                                    </TableCell>
-                                    <TableCell>
-                                        {` ${morador.email}`}
-                                    </TableCell>
-                                    <TableCell>
-                                        {morador.telefoneID}
-                                    </TableCell>
-                                    <TableCell>
-                                        {morador.Endereco?.lote}
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </Box>
-            </ImageList>
-            <TablePagination
-                component="div"
-                count={morador.length}
-                onPageChange={handlePageChange}
-                onRowsPerPageChange={handleLimitChange}
-                page={page}
-                rowsPerPage={limit}
-                rowsPerPageOptions={[5, 10, 25]}
-            />
-        </Card>
-    );
+  return (
+    <Card>
+      <ImageList
+        sx={{
+          gridAutoFlow: "column",
+          gridTemplateColumns: "repeat(auto-fit, minmax(160px,1fr)) !important",
+          gridAutoColumns: "minmax(160px, 1fr)",
+        }}
+      >
+        <Box>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Visitante</TableCell>
+                <TableCell>Entrada</TableCell>
+                <TableCell>Saída</TableCell>
+                <TableCell>Endereço</TableCell>
+
+                <TableCell>Descrição</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {morador.slice(0, limit).map((morador) => (
+                <TableRow
+                  hover
+                  key={morador.id}
+                  selected={selectedCustomerIds.indexOf(morador.id) !== -1}
+                >
+                  <TableCell>{morador.pessoas.nomePessoa}</TableCell>
+                  <TableCell>{morador.dhEntrada}</TableCell>
+                  <TableCell>{` ${morador.dhSaida}`}</TableCell>
+                  <TableCell>{morador.telefoneID}</TableCell>
+                  <TableCell>{morador.Endereco?.lote}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
+      </ImageList>
+      <TablePagination
+        component="div"
+        count={morador.length}
+        onPageChange={handlePageChange}
+        onRowsPerPageChange={handleLimitChange}
+        page={page}
+        rowsPerPage={limit}
+        rowsPerPageOptions={[5, 10, 25]}
+      />
+    </Card>
+  );
 };
